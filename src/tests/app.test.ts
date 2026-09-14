2f9d3b2636c8932e1701794fa8059b329ebd608b
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-14 20:31:45 +0700

    configure Vitest and add initial tests for health status and unknown route

---
+++ src/tests/app.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { describe, expect, it } from 'vitest';
+
+import app from '@/app.js';
+
+describe('App', () => {
+  it('should return health status', async () => {
+    const response = await request(app).get('/api/health');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      status: 'OK',
+      message: 'JobFolio API is running',
+    });
+  });
+
+  it('should return 404 for unknown route', async () => {
+    const response = await request(app).get('/api/unknown');
+
+    expect(response.status).toBe(404);
+    expect(response.body.success).toBe(false);
+  })
+});
