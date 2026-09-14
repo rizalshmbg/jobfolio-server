596315a42f19effd2f5bb0e275c05ea2fe3354e3
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-14 19:17:11 +0700

    finish logger configuration using pino

---
+++ src/config/logger.ts
@@ -0,0 +1,22 @@
+import pino from 'pino';
+import { env } from './env.js';
+
+const logger = pino(
+  env.NODE_ENV === 'development'
+    ? {
+        level: 'debug',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        },
+      }
+    : {
+        level: 'info',
+      },
+);
+
+export default logger;
