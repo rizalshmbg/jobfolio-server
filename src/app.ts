53b21334ffcff3fd9de6bd7103875456656a62d2
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-14 19:18:13 +0700

    add logger and pino-http

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { type Express, type Request, type Response } from 'express';
+import { pinoHttp } from 'pino-http';
+import logger from '@/config/logger.js';
 
 const app: Express = express();
 
+app.use(pinoHttp({ logger }));
 app.use(express.json());
 
 app.get('/api/health', (req: Request, res: Response) => {
-    res.status(200).json({
-        status: 'OK',
-        message: 'JobFolio API is running',
-    });
+  res.status(200).json({
+    status: 'OK',
+    message: 'JobFolio API is running',
+  });
 });
 
 export default app;
