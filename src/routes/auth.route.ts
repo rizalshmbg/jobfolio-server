ff6e05c2e268744a381067a93d4e5571456d4fbc
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-15 20:18:56 +0700

    add login endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,18 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { registerController } from "../controllers/auth.controller.js";
-import { validate } from "../middlewares/validate.middleware.js";
-import { registerSchema } from "../validations/auth.validations.js";
+import { validate } from '../middlewares/validate.middleware.js';
+import {
+  registerSchema,
+  loginSchema,
+} from '../validations/auth.validations.js';
+import {
+  registerController,
+  loginController,
+} from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), registerController);
+router.post('/register', validate(registerSchema), registerController);
+router.post('/login', validate(loginSchema), loginController);
 
 export default router;
