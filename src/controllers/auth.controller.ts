4094db9ef34279e23817a254ab68e18b829ced7f
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-15 20:18:32 +0700

    add login controller to handle user authentication

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from 'express';
 
-import { register } from '../services/auth.service.js';
-import type { RegisterInput } from '../validations/auth.validations.js';
+import { register, login } from '../services/auth.service.js';
+import type { RegisterInput, LoginInput } from '../validations/auth.validations.js';
 
 export const registerController: RequestHandler = async (req, res) => {
   const user = await register(req.body as RegisterInput);
@@ -10,5 +10,15 @@ export const registerController: RequestHandler = async (req, res) => {
     success: true,
     message: 'User registered successfully',
     data: user,
+  });
+};
+
+export const loginController: RequestHandler = async (req, res) => {
+  const result = await login(req.body as LoginInput);
+
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
+    data: result,
   });
 };
