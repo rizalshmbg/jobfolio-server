aa142ff458ed99bca01ffbff0683dbf6d1ee48aa
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-15 19:31:20 +0700

    add auth router integration in app setup

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,11 @@ import { pinoHttp } from 'pino-http';
 import logger from './config/logger.js';
 import { notFoundMiddleware } from './middlewares/not-found.middleware.js';
 import { errorMiddleware } from './middlewares/error.middleware.js';
+import authRouter from './routes/auth.route.js';
 
 const app: Express = express();
 
 app.use(pinoHttp({ logger }));
-
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +19,8 @@ app.get('/api/health', (_req: Request, res: Response) => {
     message: 'JobFolio API is running',
   });
 });
+
+app.use('/api/auth', authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
