f6d7902e4eef8289e239d6827f937628cd46a223
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-15 19:29:31 +0700

    add validation schema for user registration

---
+++ src/validations/auth.validations.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, "Name must be at least 2 characters")
+    .max(100, "Name must be at most 100 characters"),
+
+  email: z
+    .email('Invalid email address')
+    .trim()
+    .toLowerCase(),
+
+  password: z
+    .string()
+    .trim()
+    .min(8, "Password must be at least 8 characters")
+    .max(64, "Password must be at most 16 characters"),
+});
+
+export type RegisterInput = z.infer<typeof registerSchema>;
