f6f2c23eadae32d7cab31b3ed8b930cce2f0bdad
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-15 19:12:00 +0700

    setup prisma database to postgresql

---
+++ src/config/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client.js';
+
+import { env } from './env.js';
+
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma ;
