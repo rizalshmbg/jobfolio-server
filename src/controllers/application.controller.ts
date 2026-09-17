61caaaff7854b02d1d5ab9a9a79dd6ba67d96854
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-17 20:19:59 +0700

    add get application controller

--- src/controllers/application.controller.ts
+++ src/controllers/application.controller.ts
@@ -1,12 +1,12 @@
 import type { RequestHandler } from 'express';
 
-import { createApplication } from '../services/application.service.js';
+import {
+  createApplication,
+  getApplications,
+} from '../services/application.service.js';
 import type { CreateApplicationInput } from '../validations/application.validations.js';
 
-export const createApplicationController: RequestHandler = async (
-  req,
-  res,
-) => {
+export const createApplicationController: RequestHandler = async (req, res) => {
   const application = await createApplication(
     req.user!.userId,
     req.body as CreateApplicationInput,
@@ -18,3 +18,13 @@ export const createApplicationController: RequestHandler = async (
     data: application,
   });
 };
+
+export const getApplicationsController: RequestHandler = async (req, res) => {
+  const applications = await getApplications(req.user!.userId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Applications retrieved successfully',
+    data: applications,
+  });
+}
