c45b7f3798b2dbe81b38e344ff5a399bc794a717
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-17 21:00:35 +0700

    refactor application routes to include query validation for getApplications endpoint

--- src/routes/application.route.ts
+++ src/routes/application.route.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 
 import { createApplicationController, getApplicationsController } from '../controllers/application.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
-import { validate } from '../middlewares/validate.middleware.js';
-import { createApplicationSchema } from '../validations/application.validations.js';
+import { validate, validateQuery } from '../middlewares/validate.middleware.js';
+import { createApplicationSchema, getApplicationsQuerySchema } from '../validations/application.validations.js';
 
 const router = Router();
 
 router.post('/', authMiddleware, validate(createApplicationSchema), createApplicationController);
-router.get('/', authMiddleware, getApplicationsController);
+router.get('/', authMiddleware, validateQuery(getApplicationsQuerySchema), getApplicationsController);
 
 export default router;
