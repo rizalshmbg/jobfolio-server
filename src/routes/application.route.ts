2a66ccc6ad820d236684eb324a7904ad66e77373
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-17 20:20:08 +0700

    add getApplications route to application router

--- src/routes/application.route.ts
+++ src/routes/application.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createApplicationController } from '../controllers/application.controller.js';
+import { createApplicationController, getApplicationsController } from '../controllers/application.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 import { validate } from '../middlewares/validate.middleware.js';
 import { createApplicationSchema } from '../validations/application.validations.js';
@@ -8,5 +8,6 @@ import { createApplicationSchema } from '../validations/application.validations.
 const router = Router();
 
 router.post('/', authMiddleware, validate(createApplicationSchema), createApplicationController);
+router.get('/', authMiddleware, getApplicationsController);
 
 export default router;
