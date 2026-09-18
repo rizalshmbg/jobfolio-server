25ce03fad6d468c3859e309b76b2cc9c9c22b969
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-18 18:39:22 +0700

    add updateApplication route for modifying application details

--- src/routes/application.route.ts
+++ src/routes/application.route.ts
@@ -1,14 +1,50 @@
 import { Router } from 'express';
 
-import { createApplicationController, getApplicationByIdController, getApplicationsController } from '../controllers/application.controller.js';
+import {
+  createApplicationController,
+  getApplicationByIdController,
+  getApplicationsController,
+  updateApplicationController,
+} from '../controllers/application.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
-import { validate, validateParams, validateQuery } from '../middlewares/validate.middleware.js';
-import { applicationIdParamsSchema, createApplicationSchema, getApplicationsQuerySchema } from '../validations/application.validations.js';
+import {
+  validate,
+  validateParams,
+  validateQuery,
+} from '../middlewares/validate.middleware.js';
+import {
+  applicationIdParamsSchema,
+  createApplicationSchema,
+  getApplicationsQuerySchema,
+  updateApplicationSchema,
+} from '../validations/application.validations.js';
 
 const router = Router();
 
-router.post('/', authMiddleware, validate(createApplicationSchema), createApplicationController);
-router.get('/', authMiddleware, validateQuery(getApplicationsQuerySchema), getApplicationsController);
-router.get('/:id', authMiddleware, validateParams(applicationIdParamsSchema), getApplicationByIdController);
+router.post(
+  '/',
+  authMiddleware,
+  validate(createApplicationSchema),
+  createApplicationController,
+);
+router.get(
+  '/',
+  authMiddleware,
+  validateQuery(getApplicationsQuerySchema),
+  getApplicationsController,
+);
+router.get(
+  '/:id',
+  authMiddleware,
+  validateParams(applicationIdParamsSchema),
+  getApplicationByIdController,
+);
+router.patch(
+  '/:id',
+  authMiddleware,
+  validateParams(applicationIdParamsSchema),
+  validate(updateApplicationSchema),
+  updateApplicationController,
+);
 
 export default router;
