2149044a549783f009adade36aaac0b30b4acbce
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-18 20:56:11 +0700

    create getDashboardController to handle dashboard retrieval

---
+++ src/controllers/dashboard.controller.ts
@@ -0,0 +1,18 @@
+import type { RequestHandler } from 'express';
+import { getDashboard } from '../services/dashboard.service.js';
+
+export const getDashboardController: RequestHandler = async (req, res, next) => {
+  try {
+    const userId = req.user!.userId;
+
+    const result = await getDashboard(userId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Dashboard retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
