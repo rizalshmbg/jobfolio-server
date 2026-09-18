3530b5d4b5954c856be518e3364fd38cbdad08ee
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-18 11:18:28 +0700

    add getApplicationById route for retrieving application details by ID

--- src/routes/application.route.ts
+++ src/routes/application.route.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
-import { createApplicationController, getApplicationsController } from '../controllers/application.controller.js';
+import { createApplicationController, getApplicationByIdController, getApplicationsController } from '../controllers/application.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
-import { validate, validateQuery } from '../middlewares/validate.middleware.js';
-import { createApplicationSchema, getApplicationsQuerySchema } from '../validations/application.validations.js';
+import { validate, validateParams, validateQuery } from '../middlewares/validate.middleware.js';
+import { applicationIdParamsSchema, createApplicationSchema, getApplicationsQuerySchema } from '../validations/application.validations.js';
 
 const router = Router();
 
 router.post('/', authMiddleware, validate(createApplicationSchema), createApplicationController);
 router.get('/', authMiddleware, validateQuery(getApplicationsQuerySchema), getApplicationsController);
+router.get('/:id', authMiddleware, validateParams(applicationIdParamsSchema), getApplicationByIdController);
 
 export default router;
