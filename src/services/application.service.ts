7493ceb43c8006e679c27feadb308d660f3eab7c
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-18 18:39:01 +0700

    add updateApplication function for modifying existing application details with validation

--- src/services/application.service.ts
+++ src/services/application.service.ts
@@ -3,6 +3,7 @@ import prisma from '../config/prisma.js';
 import type {
   CreateApplicationInput,
   GetApplicationsQuery,
+  UpdateApplicationInput,
 } from '../validations/application.validations.js';
 import type { Prisma } from '../generated/prisma/client.js';
 import { AppError } from '../errors/app-error.js';
@@ -144,4 +145,83 @@ export const getApplicationById = async (
   }
 
   return application;
+};
+
+export const updateApplication = async (
+  userId: string,
+  applicationId: string,
+  data: UpdateApplicationInput,
+) => {
+  const application = await getApplicationById(userId, applicationId);
+
+  const finalSalaryMin =
+    data.salaryMin !== undefined ? data.salaryMin : application.salaryMin;
+  const finalSalaryMax =
+    data.salaryMax !== undefined ? data.salaryMax : application.salaryMax;
+
+  // validation if finalSalaryMin > finalSalaryMax
+  if (
+    finalSalaryMin !== null &&
+    finalSalaryMax !== null &&
+    finalSalaryMin > finalSalaryMax
+  ) {
+    throw new AppError(
+      400,
+      'Minimum salary cannot be greater than maximum salary',
+    );
+  }
+
+  // update application with Prisma
+  const updatedApplication = await prisma.application.update({
+    where: {
+      id: application.id,
+    },
+    data: {
+      ...(data.company !== undefined && {
+        company: data.company,
+      }),
+
+      ...(data.position !== undefined && {
+        position: data.position,
+      }),
+
+      ...(data.status !== undefined && {
+        status: data.status,
+      }),
+
+      ...(data.appliedAt !== undefined && {
+        appliedAt: data.appliedAt,
+      }),
+
+      ...(data.jobUrl !== undefined && {
+        jobUrl: data.jobUrl,
+      }),
+
+      ...(data.location !== undefined && {
+        location: data.location,
+      }),
+
+      ...(data.employmentType !== undefined && {
+        employmentType: data.employmentType,
+      }),
+
+      ...(data.workArrangement !== undefined && {
+        workArrangement: data.workArrangement,
+      }),
+
+      ...(data.salaryMin !== undefined && {
+        salaryMin: data.salaryMin,
+      }),
+
+      ...(data.salaryMax !== undefined && {
+        salaryMax: data.salaryMax,
+      }),
+
+      ...(data.notes !== undefined && {
+        notes: data.notes,
+      }),
+    },
+  });
+
+  return updatedApplication;
 };
